9152dfb062363099b183c5a0eb873eb63933ed0d
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 00:25:43 -0700

    Instructor comment

---
+++ grade.sh
@@ -0,0 +1,9 @@
+###
+# INSTRUCTOR NOTE:
+#
+# Good assessment.  However, please edit the README in place - do not create a new filename.
+# Also, you did not commit your shell file (program). Instead you commited a zip file containing
+# directories and blank files. The created files did not match the program specification.
+#
+# 6/10
+###
